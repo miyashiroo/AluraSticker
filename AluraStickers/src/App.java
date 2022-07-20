5a142bccd4ff87eb6056020d42d4276f622e258f
miyashiroo <miyashiroo@gmail.com>
2022-07-20 06:03:21 -0400

    Primeira aula da imersão java Alura

--- AluraStickers/src/App.java
+++ AluraStickers/src/App.java
@@ -1,5 +1,42 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        
+        var cliente = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response =  cliente.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+
+        // pegar os dados que interessam (titulo, poster, rating) -> parsear os dados
+        var parser = new JsonParser();
+        List<Map<String, String >> listaDeFilmes = parser.parse(body);
+             
+        // exibit e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+
+        }
+
+
+
+
+
     }
 }
